ad4eaedab82e4421139aa95a14a0b218f61d26a6
0022331 <68958328+0022331@users.noreply.github.com>
2020-07-30 00:34:42 +0900

    Add files via upload

---
+++ js/c.js
@@ -0,0 +1,17 @@
+
+function hand(quiz){
+    
+    
+
+    var message = "";
+    
+        if (quiz == 1) {
+            message = "残念！答えは２です。";
+        }else if(quiz == 2) {
+            message = "おめでとう！正解です。";
+        }else if(quiz == 3) {
+            message = "残念！答えは２です。";
+        }
+            alert(message);
+    
+}
